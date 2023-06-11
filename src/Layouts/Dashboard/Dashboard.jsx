4fe4349ec102e14940c0c49f54734319912a2be8
Rafsanfaruk <39384467+Rafsanfaruk@users.noreply.github.com>
2023-06-11 13:38:54 +0600

    instructor page add

--- src/Layouts/Dashboard/Dashboard.jsx
+++ src/Layouts/Dashboard/Dashboard.jsx
@@ -7,20 +7,35 @@ import { AiFillSetting } from "react-icons/ai";
 import { MdClass } from "react-icons/md";
 import useCart from "../../hooks/useCart";
 import useAdmin from "../../hooks/useAdmin";
-
+import useInstructor from "../../hooks/useInstructor";
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Dashboard = () => {
+  const { user, loading } = useContext(AuthContext);
   const [cart] = useCart();
+  const [isAdmin, isAdminLoading] = useAdmin();
+  const [isInstructor, isInstructorLoading] = useInstructor();
+  const location = useLocation();
 
-  
-//   const isAdmin = true;
-  const [isAdmin] = useAdmin();
+  if (loading || isAdminLoading || isInstructorLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-16 w-16 border-4 border-blue-500"></div>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 
   return (
-    <div className="drawer lg:drawer-open ">
+    <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col items-center justify-center">
-        <Outlet></Outlet>
+        <Outlet />
         <label
           htmlFor="my-drawer-2"
           className="btn btn-primary drawer-button lg:hidden"
@@ -32,33 +47,49 @@ const Dashboard = () => {
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 h-full bg-gradient-to-b from-purple-500 to-blue-500 text-black text-xl">
           {isAdmin ? (
+            // Admin section
             <>
               <li>
                 <NavLink to="/dashboard/adminhome">
-                  <FaHome></FaHome> Admin Home
+                  <FaHome /> Admin Home
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/dashboard/manageclasses">
-                  <MdClass></MdClass> Manage Classes
+                  <MdClass /> Manage Classes
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/dashboard/allusers">
-                  <FcContacts></FcContacts> Manage Users
+                  <FcContacts /> Manage Users
+                </NavLink>
+              </li>
+            </>
+          ) : isInstructor ? (
+            // Instructor section
+            <>
+              <li>
+                <NavLink to="/dashboard/instructorhome">
+                  <FaHome /> Instructor Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/myclasses">
+                  <MdClass /> My Classes
                 </NavLink>
               </li>
             </>
           ) : (
+            // User section
             <>
               <li>
                 <NavLink to="/dashboard/userhome">
-                  <FaHome></FaHome>User Home
+                  <FaHome /> User Home
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/dashboard/payment">
-                  <FaWallet></FaWallet>Payment
+                  <FaWallet /> Payment
                 </NavLink>
               </li>
               <li>
@@ -72,31 +103,31 @@ const Dashboard = () => {
               </li>
               <li>
                 <NavLink to="/dashboard/enrollclasses">
-                  <MdClass></MdClass> My Enrolled Classes
+                  <MdClass /> My Enrolled Classes
                 </NavLink>
               </li>
             </>
           )}
 
-          <div className="divider"></div>
+          <div className="divider" />
           <li>
             <NavLink to="/">
-              <FaHome></FaHome> Home
+              <FaHome /> Home
             </NavLink>
           </li>
           <li>
             <NavLink to="/dashboard/contact">
-              <FcContacts></FcContacts> Contact
+              <FcContacts /> Contact
             </NavLink>
           </li>
           <li>
             <NavLink to="/dashboard/sitting">
-              <AiFillSetting></AiFillSetting> Settings
+              <AiFillSetting /> Settings
             </NavLink>
           </li>
           <li>
             <NavLink to="dashboard/logout">
-              <FiLogOut></FiLogOut> Logout
+              <FiLogOut /> Logout
             </NavLink>
           </li>
         </ul>
--- src/Routes/PrivateInstructorRoute.jsx
+++ src/Routes/PrivateInstructorRoute.jsx
@@ -1,12 +1,14 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import useInstructor from "../hooks/useInstructor";
 
 const PrivateInstructorRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
+  const [isInstructor, isInstructorLoading] = useInstructor();
   const location = useLocation();
 
-  if (loading) {
+  if (loading || isInstructorLoading) {
     return (
       <div className="flex justify-center items-center h-screen">
         <div className="animate-spin rounded-full h-16 w-16 border-4 border-blue-500"></div>
@@ -14,7 +16,7 @@ const PrivateInstructorRoute = ({ children }) => {
     );
   }
 
-  if (user && user.role === "instructor") {
+  if (user && isInstructor) {
     return children;
   }
 
